18e412bca1e3e501a7a4b1d576df48571876c25f
mdiazfdl <mdiazfdl@gmail.com>
2023-09-12 20:31:50 -0400

    ordenando

--- src/Users/createUser.js
+++ src/Users/createUser.js
@@ -1,37 +1,14 @@
 const { v4 } = require('uuid')
 const AWS = require('aws-sdk');
+AWS.config.update({region: "eu-est-01", endpoint: "http://localhost:8000"});
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+const TableName = "userTable"
 
-// Configura AWS SDK para usar DynamoDB Local
-AWS.config.update({
-    region: "eu-est-01",
-    endpoint: "http://localhost:8000"
-});
-
-const createUser = async (event) => {
-
-    const dynamodb = new AWS.DynamoDB.DocumentClient();
+module.exports.createUser = async (event) => {
 
     const {username, password} = JSON.parse(event.body);
-    const createdAt = new Date();
     const id = v4();
 
-    const newUser ={
-        id,
-        username,
-        password,
-        createdAt
-    }
-
-    await dynamodb.put({
-        TableName: 'userTable',
-        Item: newUser
-    }).promise()
-
-    return{
-        statusCode: 200,
-        body: JSON.stringify(newUser)
-    };
-};
-module.exports = {
-    createUser,
+    const newUser =  await dynamodb.put({TableName,Item: {id,username,password}}).promise()
+    return{statusCode: 200, body: JSON.stringify({id,username,password})};
 };
